a55a05e72996c57335de64ea7fad779da9e725f8
Ali Zand <alizand9224@gmail.com>
2018-11-27 21:24:45 -0800

    28 Actions

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+
+export const createUser = (params) => (dispatch) => {
+  axios.post("/api/users/create/", {
+    params: params,
+  }).then(res => {
+    dispatch({
+      type: "CREATE_USER",
+      payload: res.data,
+    });
+  }).catch(err => console.log(err));
+};
+
+export const deleteUser = (username) => (dispatch) => {
+  axios.delete("/api/users/delete/", {
+    params: username,
+  }).then(res => {
+    dispatch({
+      type: "DELETE_USER",
+      payload: res.data,
+    });
+  }).catch(err => console.log(err));
+};
+
+export const login = (params) => (dispatch) => {
+  axios.login("/api/users/login/", {
+    params: params,
+  }).then(res => {
+    dispatch({
+      type: "LOGIN",
+      payload: res.data,
+    })
+  })
+};
+
